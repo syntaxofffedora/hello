6e158037b1d9504520534e902f3212fd1a58cade
fedora syntaxoff <syntaxofffedora@yandex.ru>
2024-10-15 21:08:06 +0300

    first commit

---
+++ hello_test.go
@@ -0,0 +1,17 @@
+package hello_test
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestPrintTo_PrintsHelloMessageToGivenWriter(t *testing.T) {
+	t.Parallel()
+	buf := new(bytes.Buffer)
+	hello.PrintTo(buf)
+	want := "Hello, world\n"
+	got := buf.String()
+	if want != got {
+		t.Errorf("want %q, got %q", want, got)
+	}
+}
